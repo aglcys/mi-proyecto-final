9b229207afae3b17bd1336dca7ebd77ef0fc9c59
aglcys <angelicayanez30@gmail.com>
2022-10-13 21:53:55 -0300

    segundo commit de mi proyecto

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from ejemplo.models import Familiar
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", 
+    {
+    "nombre":"Angelica",
+    "apellido":"Yanez",
+    })
+
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html", 
+    {
+    "nombre":nombre,
+    "apellido":apellido,
+    })
+
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html",
+    {"notas": [1,2,3,4,5,6,7,8]})
+
+def mostrar_familiares(request):
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
